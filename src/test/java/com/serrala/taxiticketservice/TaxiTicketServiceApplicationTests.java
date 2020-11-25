ac011470f14bec6738bc2d331a2389c805379cdf
Vipin Rai <vipinrai.mcs@gmail.com>
2020-11-25 14:25:50 +0530

    Initial Project Setup

---
+++ src/test/java/com/serrala/taxiticketservice/TaxiTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serrala.taxiticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiTicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
